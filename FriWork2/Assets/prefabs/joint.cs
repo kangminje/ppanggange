c72c4c69121432c89961237a8f087441ce0f403a
강민재2 <zhfem47@gmail.com>
2015-07-03 19:26:36 +0900

    pinball

---
+++ FriWork2/Assets/prefabs/joint.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class joint : MonoBehaviour {
+
+	private HingeJoint hinge;
+	private JointMotor moter;
+
+
+
+	// Use this for initialization
+	void Start () {
+		hinge = GetComponent<HingeJoint> ();
+		moter = new JointMotor ();
+		hinge.useMotor = true;
+		moter.force = 100000.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.M)) {
+			moter.targetVelocity = 100000.0f;
+
+		
+		}
+
+		if (Input.GetKeyUp (KeyCode.M)) {
+			moter.targetVelocity = -100000.0f;
+		
+			
+		}
+
+
+		hinge.motor = moter;
+	}
+}
---
+++ FriWork2/Assets/prefabs/springmm.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class springmm : MonoBehaviour {
+	private Rigidbody springBody;
+
+
+	// Use this for initialization
+	void Start () {
+		springBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			springBody.AddForce(new Vector3(0.0f,102.0f,0.0f),ForceMode.Impulse);
+		}
+	
+	}
+}
